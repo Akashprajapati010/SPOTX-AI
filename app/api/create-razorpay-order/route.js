b9bd684864e1e6d562897afc852446b6a504c96b
Akash Prajapati <akashak5654@gmail.com>
2026-09-09 21:26:09 +0530

    Remove Razorpay debug logs

--- app/api/create-razorpay-order/route.js
+++ app/api/create-razorpay-order/route.js
@@ -2,8 +2,7 @@ import Razorpay from "razorpay";
 import { NextResponse } from "next/server";
 
 // ✅ Debug logs OUTSIDE object
-console.log("KEY_ID:", process.env.RAZORPAY_KEY_ID);
-console.log("KEY_SECRET:", process.env.RAZORPAY_KEY_SECRET);
+
 
 const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
